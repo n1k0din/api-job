8ea96b2cb4f95e0242fd5334b57631ba0b6630ee
n1k0din <nik726@gmail.com>
2021-04-20 10:07:40 +0700

    Add first search vacancy

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from pprint import pprint
+
 import requests
 
 
+class HeadHunterParser:
+    def __init__(self, api_url):
+        self.api_url = api_url
+
+    def get_vacancies_by_name(self, text):
+        url = f'{self.api_url}/vacancies'
+        params = {'text': text}
+
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+
+        return response.json()
+
+
 def main():
-    pass
+    hh_parser = HeadHunterParser('https://api.hh.ru')
+    pprint(hh_parser.get_vacancies('программист'))
 
 
 if __name__ == '__main__':
