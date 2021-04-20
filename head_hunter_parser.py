0e16972a8abfbb3fc065671fdd2be20310401823
n1k0din <nik726@gmail.com>
2021-04-20 18:52:34 +0700

    Change max_pages to max_results

--- head_hunter_parser.py
+++ head_hunter_parser.py
@@ -8,10 +8,10 @@ class HeadHunterParser:
         'Москва': 1,
     }
 
-    def __init__(self, api_url, per_page=20, max_pages=5):
+    def __init__(self, api_url, per_page=20, max_results=2000):
         self.api_url = api_url
         self.per_page = per_page
-        self.max_pages = max_pages
+        self.max_results = max_results
 
     def get_vacancies(self, text, area):
         url = f'{self.api_url}/vacancies'
@@ -34,7 +34,7 @@ class HeadHunterParser:
 
             vacancies.extend(page_data['items'])
 
-            if page >= page_data['pages'] or page > self.max_pages - 1:
+            if page >= page_data['pages'] or page * self.per_page >= self.max_results - self.per_page:
                 break
 
         return page_data['found'], vacancies
