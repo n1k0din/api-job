d13e1d5b7ef3f37b8483e2616617232519ac5990
n1k0din <nik726@gmail.com>
2021-04-21 14:16:12 +0700

    Rm unnesessary ifmain

--- predict_salary.py
+++ predict_salary.py
@@ -6,7 +6,3 @@ def predict_salary(salary_from, salary_to, from_coeff=1.2, to_coeff=0.8):
         return salary_from * from_coeff
 
     return (salary_from + salary_to) / 2
-
-
-if __name__ == '__main__':
-    pass
